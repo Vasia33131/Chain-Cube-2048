e82263cc1af40330a2bbbdf10849a0f5b67de789
Vasia33131 <darkleo22365@gmail.com>
2025-08-18 13:47:33 +0300

    prototip

--- Assets/ChainCube/Scripts/Utils/CubeSpawner.cs
+++ Assets/ChainCube/Scripts/Utils/CubeSpawner.cs
@@ -7,60 +7,96 @@ namespace ChainCube.Scripts.Utils
     public class CubeSpawner : MonoBehaviour
     {
         [SerializeField] private float _spawnDelay = 0.3f;
-
         [SerializeField] private GameObject _cubePrefab;
-
         [SerializeField] private GameObject _swipeDetectorObject;
 
         private CubeDependencyInjector[] _cubeDependencies;
-        
         private ISwipeDetector _swipeDetector;
-
         private Coroutine _spawnRoutine;
-        
+
         private void Start()
         {
-            _swipeDetector = _swipeDetectorObject.GetComponent<ISwipeDetector>();
+            if (_swipeDetectorObject != null)
+            {
+                _swipeDetector = _swipeDetectorObject.GetComponent<ISwipeDetector>();
+                if (_swipeDetector != null)
+                {
+                    Subscribe();
+                }
+            }
+
             _cubeDependencies = FindObjectsOfType<CubeDependencyInjector>();
-            Subscribe();
         }
 
         private void Subscribe()
         {
-            _swipeDetector.onSwipeEnd += OnSwipeEnd;
+            if (_swipeDetector != null)
+            {
+                _swipeDetector.onSwipeEnd += OnSwipeEnd;
+            }
         }
 
         private void Unsubscribe()
         {
-            _swipeDetector.onSwipeEnd -= OnSwipeEnd;
+            if (_swipeDetector != null)
+            {
+                _swipeDetector.onSwipeEnd -= OnSwipeEnd;
+            }
         }
 
         private void OnSwipeEnd(Vector2 delta)
         {
-            if (_spawnRoutine == null)
+            if (_spawnRoutine == null && _cubePrefab != null)
+            {
                 _spawnRoutine = StartCoroutine(SpawnWithDelay());
+            }
         }
 
         private IEnumerator SpawnWithDelay()
         {
             yield return null;
             yield return new WaitForSeconds(_spawnDelay);
+
+            if (_cubePrefab == null)
+            {
+                _spawnRoutine = null;
+                yield break;
+            }
+
             var instance = Instantiate(_cubePrefab, transform.position, Quaternion.identity);
-            InjectCube(instance.gameObject);
+
+            if (instance != null)
+            {
+                InjectCube(instance.gameObject);
+            }
+
             _spawnRoutine = null;
         }
 
         private void InjectCube(GameObject cube)
         {
+            if (cube == null || _cubeDependencies == null)
+                return;
+
             foreach (var dependency in _cubeDependencies)
             {
-                dependency.Cube = cube;
+                if (dependency != null)
+                {
+                    dependency.Cube = cube;
+                }
             }
         }
 
         private void OnDestroy()
         {
             Unsubscribe();
+
+            // Stop any running coroutines when the object is destroyed
+            if (_spawnRoutine != null)
+            {
+                StopCoroutine(_spawnRoutine);
+                _spawnRoutine = null;
+            }
         }
     }
 }
